973f5c2b1abdb3b26edbdb5c5dbaa0556c28a17b
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-17 13:15:32 +0530

    feat: exposed api for py type dict hint

--- src/djresttoolkit/mail/__init__.py
+++ src/djresttoolkit/mail/__init__.py
@@ -1,3 +1,9 @@
 from ._models import Email, EmailTemplate
+from ._types import EmailDict, EmailTemplateDict
 
-__all__ = ["Email", "EmailTemplate"]
+__all__ = [
+    "Email",
+    "EmailTemplate",
+    "EmailDict",
+    "EmailTemplateDict",
+]
