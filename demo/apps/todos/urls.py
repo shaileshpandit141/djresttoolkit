0930b9b5f6292d49febf0f85c79cf8273a64370b
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-18 10:16:24 +0530

    chore: configure todos urls

---
+++ demo/apps/todos/urls.py
@@ -0,0 +1,7 @@
+"""
+URL configuration for todos app.
+"""
+
+from django.urls import path
+
+urlpatterns = []
--- demo/apps_config/urls.py
+++ demo/apps_config/urls.py
@@ -2,11 +2,13 @@
 URL configuration for apps_config project.
 """
 
+from apps.todos import urls as todos_urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/auth/token/", obtain_auth_token, name="api_token_auth"),
+    path("api/v1/todos/", include(todos_urls))
 ]
