9b0408768dde67b72dbe24fd47adfd52a9a3b174
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-19 10:56:53 +0530

    feat: build base dbseed model

--- src/djresttoolkit/dbseed/models/__init__.py
+++ src/djresttoolkit/dbseed/models/__init__.py
@@ -1,3 +1,4 @@
+from ._base_dbseed_model import BaseDBSeedModel
 from ._generator import generator
 
-__all__ = ["generator"]
+__all__ = ["BaseDBSeedModel", "generator"]
---
+++ src/djresttoolkit/dbseed/models/_base_dbseed_model.py
@@ -0,0 +1,45 @@
+from typing import Any
+
+from django.db.models import ForeignKey, ManyToManyField, Model, OneToOneField
+from pydantic import BaseModel
+
+
+class BaseDBSeedModel(BaseModel):
+    """
+    Base class for all fake data models.
+    Each subclass must define a `django_model` attribute.
+    """
+
+    django_model: Model | None = None
+
+    @classmethod
+    def create_instance(cls) -> tuple[dict[str, Any], list[ManyToManyField[Any, Any]]]:
+        """Handle ForeignKey, OneToOneField and ManyToMany relationship."""
+        if cls.django_model is None:
+            raise ValueError("django_model must be set")
+
+        data = cls().model_dump()
+
+        # Handle ForeignKey and OneToOneField
+        for field in cls.django_model._meta.get_fields():
+            if isinstance(field, (ForeignKey, OneToOneField)):
+                rel_model = field.remote_field.model
+                if rel_model.objects.exists():
+                    # For OneToOne, must ensure unique (pick unused relation)
+                    if isinstance(field, OneToOneField):
+                        used_ids = cls.django_model.objects.values_list(
+                            field.name, flat=True
+                        )
+                        available = rel_model.objects.exclude(pk__in=used_ids)
+                        if available.exists():
+                            data[field.name] = available.order_by("?").first()
+                    else:  # Normal ForeignKey
+                        data[field.name] = rel_model.objects.order_by("?").first()
+
+        # Collect ManyToMany fields
+        m2m_fields: list[ManyToManyField[Any, Any]] = [
+            field
+            for field in cls.django_model._meta.get_fields()
+            if isinstance(field, ManyToManyField)
+        ]
+        return data, m2m_fields
