bed818215293bea14e13b7b8715f68d93df4bbe7
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-19 11:23:36 +0530

    feat: redesign base db model class

--- src/djresttoolkit/dbseed/models/_base_dbseed_model.py
+++ src/djresttoolkit/dbseed/models/_base_dbseed_model.py
@@ -1,33 +1,45 @@
+from __future__ import annotations
+
 from typing import Any
 
 from django.db.models import ForeignKey, ManyToManyField, Model, OneToOneField
-from pydantic import BaseModel
+from pydantic import BaseModel, PrivateAttr
 
 
 class BaseDBSeedModel(BaseModel):
     """
     Base class for all fake data models.
-    Each subclass must define a `django_model` attribute.
+    Each subclass must define a `model` attribute.
     """
 
-    django_model: Model | None = None
+    class Meta:
+        model: type[Model]
+
+    _meta: type[Meta] = PrivateAttr()
+
+    def __init_subclass__(cls, **kwargs: Any) -> None:
+        super().__init_subclass__(**kwargs)
+        if not hasattr(cls, "Meta") or not hasattr(cls.Meta, "model"):
+            raise TypeError(
+                f"{cls.__name__} must define a Meta class with django model"
+            )
+        cls._meta = cls.Meta
 
     @classmethod
     def create_instance(cls) -> tuple[dict[str, Any], list[ManyToManyField[Any, Any]]]:
         """Handle ForeignKey, OneToOneField and ManyToMany relationship."""
-        if cls.django_model is None:
-            raise ValueError("django_model must be set")
 
+        # dump pydantic model to python dict
         data = cls().model_dump()
 
         # Handle ForeignKey and OneToOneField
-        for field in cls.django_model._meta.get_fields():
+        for field in cls._meta.model._meta.get_fields():
             if isinstance(field, (ForeignKey, OneToOneField)):
                 rel_model = field.remote_field.model
                 if rel_model.objects.exists():
                     # For OneToOne, must ensure unique (pick unused relation)
                     if isinstance(field, OneToOneField):
-                        used_ids = cls.django_model.objects.values_list(
+                        used_ids = cls._meta.model.objects.values_list(
                             field.name, flat=True
                         )
                         available = rel_model.objects.exclude(pk__in=used_ids)
@@ -39,7 +51,7 @@ class BaseDBSeedModel(BaseModel):
         # Collect ManyToMany fields
         m2m_fields: list[ManyToManyField[Any, Any]] = [
             field
-            for field in cls.django_model._meta.get_fields()
+            for field in cls._meta.model._meta.get_fields()
             if isinstance(field, ManyToManyField)
         ]
         return data, m2m_fields
