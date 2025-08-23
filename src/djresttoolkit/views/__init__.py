6d8a4b5f3261125e2064b1d5a040cd32df3a2c33
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-23 14:39:32 +0530

    refactor: change suffix to apiview

--- src/djresttoolkit/views/__init__.py
+++ src/djresttoolkit/views/__init__.py
@@ -1,5 +1,5 @@
 from ._exceptions import exception_handler
-from ._api_views._choice_fields_api_view import ChoiceFieldsAPIView
+from ._api_views._choice_fields_apiview import ChoiceFieldsAPIView
 
 __all__ = [
     "exception_handler",
